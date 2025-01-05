e85ed83d9cef593f0a4038be7cc1fadb4d4e60c6
unknown <ddiazny919@gmail.com>
2025-01-05 16:47:06 -0500

    latest

---
+++ TODO.md
@@ -0,0 +1,12 @@
+1. User Management
+- Authentication Service: Handles user login/logout, session management, and password reset.
+- Authorization Service: Manages roles, permissions, and access control (e.g., Role-Based Access Control or RBAC, Attribute-Based Access Control or ABAC).
+- User Profile Service: Stores and manages user details like name, contact info, and preferences.
+- Account Management Service: Handles account-level data for organizations, teams, or individuals.
+2. Billing and Payments
+- Subscription Management Service: Handles plans, tiers, upgrades/downgrades, and trials.
+- Payment Processing Service: Integrates with payment gateways (e.g., Stripe, PayPal) to handle transactions.
+- Refunds/Adjustments Service: Handles refunds, credits, or adjustments to customer payments.
+3. Notification and Communication
+- Email Service: Sends transactional emails (e.g., sign-up confirmation, password reset) and marketing emails.
+- Webhooks Service: Sends notifications to external systems via webhooks for real-time updates.
--- users/model.go
+++ users/model.go
@@ -5,13 +5,44 @@ import (
 	"time"
 )
 
-type Admin bool
-
 type User struct {
-	ID        int       `json:"id"`
-	Email     string    `json:"email"`
-	Verified  bool      `json:"verified"`
-	Admin     Admin     `json:"admin"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID        int       `json:"id" validate:"required"`
+	Verified  bool      `json:"verified" validate:"required"`
+	CreatedAt time.Time `json:"created_at" validate:"required"`
+	UpdatedAt time.Time `json:"updated_at" validate:"required"`
+
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+
+	ContactInfo ContactInfo `json:"contact_info" validate:"required"`
+	Address     Address     `json:"address" validate:"required"`
+
+	Preferences Preferences `json:"preferences" validate:"required"`
+	Account     Account     `json:"account" validate:"required"`
+}
+
+type ContactInfo struct {
+	Email string `json:"email" validate:"required,email"`
+	Phone string `json:"phone"`
+}
+
+type Address struct {
+	Street  string `json:"street"`
+	City    string `json:"city"`
+	State   string `json:"state"`
+	Zip     string `json:"zip"`
+	Country string `json:"country"`
+}
+
+type Preferences struct {
+	Language string `json:"language"`
+	Timezone string `json:"timezone"`
+}
+
+type Account struct {
+	ID int `json:"id" validate:"required"`
+
+	MaxUsers  int       `json:"max_users" validate:"required"`
+	CreatedAt time.Time `json:"created_at" validate:"required"`
+	UpdatedAt time.Time `json:"updated_at" validate:"required"`
 }
